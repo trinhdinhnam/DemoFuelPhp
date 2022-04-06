d453e5bfdf5f92e83768f24c049c208a5f196132
trinhdinhnam16898 <namtd@vnext.com.vn>
2022-04-06 14:59:11 +0700

    Second commit

--- fuel/app/views/posts/change.php
+++ fuel/app/views/posts/change.php
@@ -1,6 +1,6 @@
-<h2><?php isset($student->id) ? 'Update Student' : 'Add Student' ?></h2>
-<?php echo Form::open(isset($student->id) ? '/posts/edit/<?php echo $student->id; ?>' : '/posts/add'); ?>
-<div class="form-group" style="width: 80%; margin-left: 40px">
+<h2><?php echo isset($student->id) ? 'Update Student' : 'Add Student' ?></h2>
+<?php echo Form::open(isset($student->id) ? '/posts/change/<?php echo $student->id; ?>' : '/posts/change'); ?>
+<div class="student_name form-group" style="width: 80%; margin-left: 40px">
     <?php echo Form::label('Name', 'name'); ?>
     <?php echo Form::input('name', Input::post('name', isset($student) ? $student->name : ''), array('class' => 'form-control')); ?>
 </div>
@@ -11,5 +11,5 @@
 <div class="actions" style="width: 80%; margin-left: 40px">
     <?php echo Form::submit('save'); ?>
 </div>
-<input type="hidden" name="student_id" value="<?php echo $student->id; ?>">
+<input type="hidden" name="student_id" value="<?php echo isset($student->id) ? $student->id : '' ?>">
 <?php echo Form::close(); ?>
